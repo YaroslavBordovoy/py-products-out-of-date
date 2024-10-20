705ce659e6ff37a9ec4d2d13e8243520accc4c6b
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-10-20 18:13:56 +0300

    Fixed flake8

--- app/test_main.py
+++ app/test_main.py
@@ -5,25 +5,27 @@ import pytest
 
 from app.main import outdated_products
 
+
 @pytest.fixture()
 def products() -> list[dict]:
     yield [
-            {
-                "name": "salmon",
-                "expiration_date": datetime.date(2022, 2, 10),
-                "price": 600
-            },
-            {
-                "name": "chicken",
-                "expiration_date": datetime.date(2022, 2, 5),
-                "price": 120
-            },
-            {
-                "name": "duck",
-                "expiration_date": datetime.date(2022, 2, 1),
-                "price": 160
-            }
-        ]
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]
+
 
 @pytest.mark.parametrize(
     "mocked_time,expected",
@@ -38,7 +40,6 @@ def products() -> list[dict]:
 def test_outdated_products(products: list[dict],
                            mocked_time: datetime.date,
                            expected: list) -> None:
-
     with mock.patch("app.main.datetime.date") as mock_date:
         mock_date.today.return_value = mocked_time
         assert outdated_products(products) == expected
